84dbb70bdbf2b781bee8329b7fc1fcb61cac06db
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-06-21 21:32:41 +0200

    t

--- testing-docker/build_package.sh
+++ testing-docker/build_package.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Check if all required environment variables are set
+if [ -z ${PLATFORM+x} ]; then echo "PLATFORM is unset"; exit 1; fi
+if [ -z ${COMPILER_VERSION+x} ]; then echo "COMPILER_VERSION is unset"; exit 1; fi
 
 echo building package ${PLATFORM} with compiler version ${COMPILER_VERSION}
